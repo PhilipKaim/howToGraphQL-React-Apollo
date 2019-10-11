49afed10afc2fa05bc4d3f5a84187b64420dbaf6
Chillup <philipkaim@gmail.com>
2019-10-11 11:51:28 -0400

    update store after mutations and add filtering

--- src/index.js
+++ src/index.js
@@ -5,8 +5,9 @@ import App from './components/App'
 import * as serviceWorker from './serviceWorker'
 import { BrowserRouter } from 'react-router-dom'
 import { setContext } from 'apollo-link-context'
-
-// 1
+import { split } from 'apollo-link'
+import { WebSocketLink } from 'apollo-link-ws'
+import { getMainDefinition } from 'apollo-utilities'
 import { ApolloProvider } from 'react-apollo'
 import { ApolloClient } from 'apollo-client'
 import { createHttpLink } from 'apollo-link-http'
@@ -14,8 +15,6 @@ import { InMemoryCache } from 'apollo-cache-inmemory'
 
 import { AUTH_TOKEN } from './constants'
 
-
-// 2
 const httpLink = createHttpLink({
   uri: 'http://localhost:4000'
 })
@@ -30,13 +29,30 @@ const authLink = setContext((_, { headers }) => {
   }
 })
 
-// 3
+const wsLink = new WebSocketLink({
+  uri: `ws://localhost:4000`,
+  options: {
+    reconnect: true,
+    connectionParams: {
+      authToken: localStorage.getItem(AUTH_TOKEN),
+    }
+  }
+})
+
+const link = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query)
+    return kind === 'OperationDefinition' && operation === 'subscription'
+  },
+  wsLink,
+  authLink.concat(httpLink)
+)
+
 const client = new ApolloClient({
   link: authLink.concat(httpLink),
   cache: new InMemoryCache()
 })
 
-// 4
 ReactDOM.render(
   <BrowserRouter>
     <ApolloProvider client={client}>
